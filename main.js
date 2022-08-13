512c5535456df85a95c42fb76e1d1718bad7bc57
Angelo Chaknis <angelo@Angelos-MacBook-Pro.local>
2022-08-12 21:13:39 -0700

    added onkeydown event for listening on the Enter key while in the input field

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const myButton = document.getElementsByClassName("addBtn")[0];
 const myUL = document.getElementById("myUL");
 
 
+// click event in which adds new list items to the ul.
 myButton.addEventListener("click", () => {
     console.log('click')
     newListItem = myInput.value;
@@ -14,6 +15,34 @@ myButton.addEventListener("click", () => {
     span.appendChild(document.createTextNode("x"));
     span.classList.add("close");
     li.appendChild(span);
-    myUL.appendChild(li);
-    alert('Added list item')
-})
+    if (confirm("Would you like to add this list item?") == true) {
+        console.log("Item Added!");
+        myUL.appendChild(li);
+      } else {
+        console.log("Added will not be added..");
+        li = null;
+      } 
+    }
+)
+
+// when user is in input box, pressing enter; creates a new list item.
+myInput.onkeydown = (event) => {
+    if(event.key === 'Enter') {
+        newListItem = myInput.value;
+    console.log(`${newListItem}`);
+    myInput.value = '';
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(newListItem));
+    let span = document.createElement("span");
+    span.appendChild(document.createTextNode("x"));
+    span.classList.add("close");
+    li.appendChild(span);
+    if (confirm("Would you like to add this list item?") == true) {
+        console.log("Item Added!");
+        myUL.appendChild(li);
+      } else {
+        console.log("Added will not be added..");
+        li = null;
+      } 
+    }
+}
